6e0276ef9dc8a037359b0bd297a4c4ea3a8504ff
mletunenko <mletunenko@gmail.com>
2025-04-14 20:47:07 +0300

    приведение ролей к enum

---
+++ src/alembic/versions/2025_04_11_2234-a80e58f00f4c_enum_roles.py
@@ -0,0 +1,59 @@
+"""enum roles
+
+Revision ID: a80e58f00f4c
+Revises: 1fcb06ab8cff
+Create Date: 2025-04-11 22:34:51.821437
+
+"""
+from datetime import datetime, timezone
+import uuid
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy import table, column, UUID, String, Boolean, TIMESTAMP
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision: str = 'a80e58f00f4c'
+down_revision: Union[str, None] = '1fcb06ab8cff'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.drop_constraint('fk_profiles_role_id_roles', 'profiles', type_='foreignkey')
+    op.drop_column('profiles', 'role_id')
+    op.drop_table('roles')
+    op.execute("CREATE TYPE role AS ENUM ('BASIC', 'SUBSCRIBER', 'ADMIN')")
+    op.add_column('profiles', sa.Column('role', sa.Enum('BASIC', 'SUBSCRIBER', 'ADMIN', name='role'), nullable=False))
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_column('profiles', 'role')
+    op.execute("DROP TYPE role")
+    op.create_table('roles',
+    sa.Column('id', sa.UUID(), autoincrement=False, nullable=False),
+    sa.Column('created_at', postgresql.TIMESTAMP(timezone=True), autoincrement=False, nullable=False),
+    sa.Column('title', sa.VARCHAR(), autoincrement=False, nullable=False),
+    sa.Column('system_role', sa.BOOLEAN(), autoincrement=False, nullable=True),
+    sa.PrimaryKeyConstraint('id', name='pk_roles'),
+    sa.UniqueConstraint('title', name='uq_roles_title')
+    )
+    roles_table = table(
+        "roles",
+        column("id", UUID),
+        column("title", String),
+        column("system_role", Boolean),
+        column("created_at", TIMESTAMP(timezone=True)),
+    )
+    op.bulk_insert(roles_table, [
+        {"id": str(uuid.uuid4()), "title": "basic", "system_role": False, "created_at": datetime.now(timezone.utc)},
+        {"id": str(uuid.uuid4()), "title": "subscriber", "system_role": False, "created_at": datetime.now(timezone.utc)},
+        {"id": str(uuid.uuid4()), "title": "admin", "system_role": True, "created_at": datetime.now(timezone.utc)},
+    ])
+    op.add_column('profiles', sa.Column('role_id', sa.UUID(), autoincrement=False, nullable=True))
+    op.create_foreign_key('fk_profiles_role_id_roles', 'profiles', 'roles', ['role_id'], ['id'], ondelete='SET NULL')
+
